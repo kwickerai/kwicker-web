68144ec53dc0ab7e8f4073dad139fd32777a787f
Ashiq <ashiqabdulr@gmail.com>
2026-05-24 16:37:04 +0400

    Replace edit with Copy/Delete long-press menu

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "45529561" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "490202373" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
