22f7f238c66dd7b7922387e58a2c4def8ac49d85
Ashiq <ashiqabdulr@gmail.com>
2026-06-07 11:27:46 +0400

    feat: Needs You badge + intervention banner

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "127842613" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2110944470" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
